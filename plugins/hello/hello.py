5dedb21a1df179cd4907431050d167ffb1e36ea2
asoefsh 1 <asoefsh@qq.com>
2023-07-10 14:33:38 +0000

    1

--- plugins/hello/hello.py
+++ plugins/hello/hello.py
@@ -38,19 +38,40 @@ class Hello(Plugin):
             e_context.action = EventAction.BREAK  # 事件结束，进入默认处理逻辑
             return
 
-        if e_context["context"].type == ContextType.PATPAT:
-            e_context["context"].type = ContextType.TEXT
-            msg: ChatMessage = e_context["context"]["msg"]
-            e_context["context"].content = f"请你随机使用一种风格介绍你自己，并告诉用户输入#help可以查看帮助信息。"
-            e_context.action = EventAction.BREAK  # 事件结束，进入默认处理逻辑
-            return
-        
+        # if e_context["context"].type == ContextType.PATPAT:
+        #     e_context["context"].type = ContextType.TEXT
+        #     msg: ChatMessage = e_context["context"]["msg"]
+        #     e_context["context"].content = f"请你随机使用一种风格介绍你自己，并告诉用户输入#help可以查看帮助信息。"
+        #     e_context.action = EventAction.BREAK  # 事件结束，进入默认处理逻辑
+        #     return
+                    
+        # if e_context["context"].type == ContextType.ADD_FRIEND:
+        #     e_context["context"].type = ContextType.TEXT
+        #     msg: ChatMessage = e_context["context"]["msg"]
+        #     e_context["context"].content = f"请你随机使用一种风格说一句问候语来欢迎新用户添加你为好友。再请你随机使用一种风格介绍你自己。"
+        #     e_context.action = EventAction.BREAK  # 事件结束，进入默认处理逻辑
+        #     return
+
         if e_context["context"].type == ContextType.ADD_FRIEND:
-            e_context["context"].type = ContextType.TEXT
+            reply = Reply()
+            reply.type = ReplyType.TEXT
             msg: ChatMessage = e_context["context"]["msg"]
-            e_context["context"].content = f"请你随机使用一种风格说一句问候语来欢迎新用户添加你为好友。再请你随机使用一种风格介绍你自己。"
-            e_context.action = EventAction.BREAK  # 事件结束，进入默认处理逻辑
-            return
+            reply.content = f"你好, {msg.from_user_nickname}！\n" \
+               "我是ALLinAI班主任的数字分身，任何关于数字分身和ChatGPT账号相关业务都可以与我交流。\n" \
+               "1、链接页有多种联系我们的方式，包括班主任本人企业微信： \n" \
+               "https://mp.weixin.qq.com/s/1v5CfgsUiUhdWKipUmJY5g\n" \
+               "2、数字分身详细介绍：\n" \
+               "https://mp.weixin.qq.com/s/618i8cX--qYKvLbaHgZipA\n" \
+               "3、ChatGPT账号服务和AI新人速成班培训介绍：\n" \
+               "https://mp.weixin.qq.com/s/7IyZF0j_nl9urszadOB_PA\n" \
+               "4、体验数字分身和ChatGPT中文站：\n" \
+               "https://gpt.chayinzi.biz\n" \
+               "体验方法：\n" \
+               "注册登录>应用>应用市场>选择相关数字分身或ChatGPT中文\n" \
+               "更多，请从“ALLinAI商业先机”公众号了解。\n" \
+               "任何问题，开始与我交流吧"
+            e_context["reply"] = reply
+            e_context.action = EventAction.BREAK_PASS  # 事件结束，并跳过处理context的默认逻辑
 
         content = e_context["context"].content
         logger.debug("[Hello] on_handle_context. content: %s" % content)
